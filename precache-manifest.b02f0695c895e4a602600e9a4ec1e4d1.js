9e268e85c92cc214e1bdd480ebe522a842b0cc4d
S.Lazarenko <ajaxamsterdam878@gmail.com>
2020-03-17 23:01:48 +0200

    Updates

---
+++ precache-manifest.b02f0695c895e4a602600e9a4ec1e4d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d829bd03e14bd26fd5beef6d2c261ed8",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "9184b70e0af3570091bf",
+    "url": "/goit-react-hw-02-feedback/static/css/main.653310a0.chunk.css"
+  },
+  {
+    "revision": "c9c941c70a21667db538",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf3f5f8f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.cf3f5f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9184b70e0af3570091bf",
+    "url": "/goit-react-hw-02-feedback/static/js/main.eb01890c.chunk.js"
+  },
+  {
+    "revision": "f59aa7dbc0213e80a103",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb39d68.js"
+  }
+]);
